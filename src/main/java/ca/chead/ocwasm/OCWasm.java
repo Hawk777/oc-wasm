9ea56ae5b36b3f868af06ad478272bf7abe1b9c6
Christopher Head <chead@chead.ca>
2022-11-18 01:06:52 -0800

    Bump version number

--- src/main/java/ca/chead/ocwasm/OCWasm.java
+++ src/main/java/ca/chead/ocwasm/OCWasm.java
@@ -65,7 +65,7 @@ public final class OCWasm {
 	/**
 	 * The mod version.
 	 */
-	public static final String VERSION = "1.12.2-0.3.0";
+	public static final String VERSION = "1.12.2-0.4.0";
 
 	/**
 	 * The number of milliseconds per second.
